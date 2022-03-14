30b580a4012b941538f6ba7c27ed85438e4bd18c
hughdide <32156980+hughdide@users.noreply.github.com>
2022-03-14 23:40:36 +0100

    Subida inicial

---
+++ WPFBalrial/Paginas/UsuIns.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WPFBalrial.Paginas
+{
+    /// <summary>
+    /// Lógica de interacción para UsuIns.xaml
+    /// </summary>
+    public partial class UsuIns : Page
+    {
+        public UsuIns()
+        {
+            InitializeComponent();
+            btCancelar.Click += BtCancelar_Click;
+        }
+
+        private void BtCancelar_Click(object sender, RoutedEventArgs e)
+        {
+            if (this.NavigationService.CanGoBack)
+            {
+                this.NavigationService.GoBack();
+            }
+        }
+    }
+}
