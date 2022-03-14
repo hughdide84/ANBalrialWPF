78f2e5027a942d6a9b1f10b238de57fb68bb85f5
hughdide <32156980+hughdide@users.noreply.github.com>
2022-03-14 23:46:37 +0100

    Subida inicial

---
+++ WPFBalrial/DTOs/UsuarioDTO.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WPFBalrial.DTOs
+{
+    public class UsuarioDTO
+    {
+        public int Id { get; set; }
+        public string Nombre { get; set; }
+        public string Apellidos { get; set; }
+        public string Login { get; set; }
+    }
+}
