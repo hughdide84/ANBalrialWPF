709aead0631d5eb1acc605c191090c738357a7f0
hughdide <32156980+hughdide@users.noreply.github.com>
2022-03-23 20:46:20 +0100

    Enganchar informe a API

--- WPFBalrial/Paginas/InfProyecto.xaml.cs
+++ WPFBalrial/Paginas/InfProyecto.xaml.cs
@@ -27,7 +27,6 @@ namespace WPFBalrial.Paginas
     public partial class InfProyecto : Page
     {
 
-
         private int idProyecto = 1;
         public InfProyecto()
         {
@@ -40,19 +39,75 @@ namespace WPFBalrial.Paginas
             var model = new PlotModel();
 
             var s1 = new BarSeries { Title = "Necesarios", FillColor = OxyColors.IndianRed, StrokeColor = OxyColors.Black, StrokeThickness = 1 };
-            s1.Items.Add(new BarItem { Value = 19 });
-            s1.Items.Add(new BarItem { Value = 22 });
-            s1.Items.Add(new BarItem { Value = 18 });
-
             var s2 = new BarSeries { Title = "Asignados", FillColor = OxyColors.Green, StrokeColor = OxyColors.Black, StrokeThickness = 1 };
-            s2.Items.Add(new BarItem { Value = 12 });
-            s2.Items.Add(new BarItem { Value = 13 });
-            s2.Items.Add(new BarItem { Value = 15 });
-
             var categoryAxis = new CategoryAxis { Position = AxisPosition.Left };
-            categoryAxis.Labels.Add("10:00-12:00");
-            categoryAxis.Labels.Add("12:00-10:00");
-            categoryAxis.Labels.Add("14:00-16:00");
+
+            // Llamada a endpoint para obtener los solicitados
+            try
+            {
+                using (var client = new HttpClient())
+                {
+                    client.BaseAddress = new Uri("https://www.galsoftpre.es/apibalrial/");
+                    client.DefaultRequestHeaders.Accept.Clear();
+                    client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+                    client.Timeout = TimeSpan.FromSeconds(Convert.ToDouble(1000000));
+                    HttpResponseMessage response = client.GetAsync("api/informes/proyecto/"+ idProyecto.ToString() + "/solicitadosTurno").Result;
+
+                    if (response.IsSuccessStatusCode)
+                    {                   
+                        IEnumerable<InformeDTO> resultados = response.Content.ReadAsAsync<IEnumerable<InformeDTO>>().Result;
+
+                        foreach(InformeDTO informeDTO in resultados)
+                        {
+                            categoryAxis.Labels.Add(informeDTO.dato);
+                            s1.Items.Add(new BarItem { Value = informeDTO.valor });
+                        }
+                    }
+                    else
+                    {
+                        tbAvisos.Text = "Se ha producido un error";
+                        tbAvisos.Foreground = Brushes.White;
+                        tbAvisos.Background = Brushes.Crimson;
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+
+            // Llamada a endpoint para obtener los asignados
+            try
+            {
+                using (var client = new HttpClient())
+                {
+                    client.BaseAddress = new Uri("https://www.galsoftpre.es/apibalrial/");
+                    client.DefaultRequestHeaders.Accept.Clear();
+                    client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+                    client.Timeout = TimeSpan.FromSeconds(Convert.ToDouble(1000000));
+                    HttpResponseMessage response = client.GetAsync("api/informes/proyecto/" + idProyecto.ToString() + "/ocupacionTurno").Result;
+
+                    if (response.IsSuccessStatusCode)
+                    {
+                        IEnumerable<InformeDTO> resultados = response.Content.ReadAsAsync<IEnumerable<InformeDTO>>().Result;
+
+                        foreach (InformeDTO informeDTO in resultados)
+                        {
+                            s2.Items.Add(new BarItem { Value = informeDTO.valor });
+                        }
+                    }
+                    else
+                    {
+                        tbAvisos.Text = "Se ha producido un error";
+                        tbAvisos.Foreground = Brushes.White;
+                        tbAvisos.Background = Brushes.Crimson;
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
 
             var valueAxis = new LinearAxis { Position = AxisPosition.Bottom, MinimumPadding = 0, MaximumPadding = 0.06, AbsoluteMinimum = 0 };
             model.Series.Add(s2);
